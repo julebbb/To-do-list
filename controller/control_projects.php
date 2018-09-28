828eac1beb9a61996be6fe57a0b924dbfc1018ac
Justine Lebbrecht <justinelebbrecht@hotmail.fr>
2018-09-28 11:40:33 +0200

    add footer and separate in view element and display formulaire for add project

--- controller/control_projects.php
+++ controller/control_projects.php
@@ -5,6 +5,47 @@ $descript = "Acceuil du site";
 require('model/model_projects.php');
 require('view/view_projects.php');
 
+//begin a session if GET have open=true else he destroy session if false
+if (isset($_GET['open']) AND ! empty($_GET['open'])) {
+  if ($_GET['open'] == 'true') {
+    session_start();
+    $_SESSION['open'] = $_GET['open'];
+  }
 
+   if ($_GET['open'] == 'false') {
+
+    session_start();
+    session_destroy();
+    $_SESSION['open'] = "";
+  }
+}
+
+//display head with title
+echo headHTML($title, $descript);
+//display all projects
+echo viewProjects($data);
+
+if (isset($_SESSION['open'])) {
+
+  if ($_SESSION['open'] == 'true') {
+
+    //display form for add project
+    echo formProjects();
+
+   } else {
+
+     //display button for display form
+      echo addButtonProject();
+
+   }
+
+ } else {
+
+   //display button for display form
+    echo addButtonProject();
+
+ }
+
+require('view/footer.php');
 
  ?>
--- view/view_projects.php
+++ view/view_projects.php
@@ -1,4 +1,52 @@
 <?php
+function headHTML($title, $descript) {
+  ob_start(); ?>
+  <!doctype html>
+  <html class="no-js" lang="fr">
+
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <title><?php echo $title; ?></title>
+      <meta name="description" content="<?php echo $descript; ?>">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+      <link rel="manifest" href="site.webmanifest">
+      <link rel="apple-touch-icon" href="icon.png">
+      <!-- Place favicon.ico in the root directory -->
+      <!--32x32 le favicon -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+      <link rel="stylesheet" href="public/css/normalize.css">
+      <link rel="stylesheet" href="public/css/main.css">
+    </head>
+
+    <body class="container">
+
+    <?php
+    $content = ob_get_clean();
+    return $content;
+}
+
+
+function viewProjects($data)  {
+  ob_start(); ?>
+
+  <section class="row">
+    <?php
+    while ($result = $data->fetch()) {
+      echo displayhtml($result);
+    }
+    ?>
+
+  </section>
+
+  <?php
+  $content = ob_get_clean();
+  return $content;
+
+}
+
 
 function displayhtml($result) {
   ob_start(); ?>
@@ -14,53 +62,35 @@ function displayhtml($result) {
    return $content;
 }
 
- ?>
+function addButtonProject() {
+    ob_start(); ?>
+      <a href="index.php?open=true" class="addproject">
+        <p>Ajouter un projet</p>
+        <i class="fas fa-plus"></i>
+      </a>
 
- <!doctype html>
- <html class="no-js" lang="fr">
+    <?php
+    $content = ob_get_clean();
+    return $content;
+}
 
-   <head>
-     <meta charset="utf-8">
-     <meta http-equiv="x-ua-compatible" content="ie=edge">
-     <title><?php echo $title; ?></title>
-     <meta name="description" content="<?php echo $descript; ?>">
-     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+function formProjects() {
+  ob_start(); ?>
 
-     <link rel="manifest" href="site.webmanifest">
-     <link rel="apple-touch-icon" href="icon.png">
-     <!-- Place favicon.ico in the root directory -->
-     <!--32x32 le favicon -->
-     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
-     <link rel="stylesheet" href="public/css/normalize.css">
-     <link rel="stylesheet" href="public/css/main.css">
-   </head>
+  <section>
+    <div class="">
+      <a href="index.php?open=false">Annuler</a>
+     <form class="" action="index.php" method="post">
+       <input type="text" name="name" value="">
+       <textarea name="description" rows="8" cols="80"></textarea>
+       <input type="date" name="deadline" value="">
+       <input type="submit" name="envoie" value="Envoyer">
+     </form>
+    </div>
 
-   <body class="container">
-     <?php //require('header.php'); ?>
-     <section class="row">
-       <?php
-       while ($result = $data->fetch()) {
-         echo displayhtml($result);
-       }
-       ?>
+  </section>
 
-     </section>
-
-     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
-     <script src="js/vendor/modernizr-3.6.0.min.js"></script>
-     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-     <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
-     <script src="js/plugins.js"></script>
-     <script src="js/main.js"></script>
-
-     <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-     <script>
-       window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
-       ga('create', 'UA-XXXXX-Y', 'auto'); ga('send', 'pageview')
-     </script>
-     <script src="https://www.google-analytics.com/analytics.js" async defer></script>
-   </body>
-
-   </html>
+  <?php
+  $content = ob_get_clean();
+  return $content;
+}
